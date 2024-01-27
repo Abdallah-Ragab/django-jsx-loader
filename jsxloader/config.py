10fc2dc902433d3036f2c1b1593be8df7ac96523
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-27 18:46:58 +0200

    Refactor config_dict assignment in load_config function

--- jsxloader/config.py
+++ jsxloader/config.py
@@ -14,9 +14,9 @@ def load_config():
         config_dict = settings.JSX_LOADER_CONFIG
         return dict_to_config(config_dict)
     else:
-        config = Config()
+        return Config()
 
 def dict_to_config(config_dict: dict) -> Config:
     default_config = Config()
-    config_dict = filter(lambda key: hasattr(default_config, key), config_dict)
+    config_dict = {key: value for key, value in config_dict.items() if hasattr(default_config, key)}
     return Config(**config_dict)
