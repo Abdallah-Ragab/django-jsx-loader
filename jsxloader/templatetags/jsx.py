69bfd0baf3811804d9a8b1ca1afafb80e7ee5b6d
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-27 19:36:36 +0200

    Add JSX component tags and nodes

--- jsxloader/templatetags/jsx.py
+++ jsxloader/templatetags/jsx.py
@@ -1,16 +1,36 @@
 from django import template
-from ..nodes import JsxNode
+from ..nodes import JsxNode, JSXFileComponentNode, JSXInlineComponentNode, JSXSyntaxNode
 
 
 register = template.Library()
 
-@register.tag(name="jsx")
+@register.tag(name="JSX")
 def do_jsx(parser, token):
-    nodelist = parser.parse(("endjsx",))
+    nodelist = parser.parse(("endJSX",))
     parser.delete_first_token()
-    return JsxNode(nodelist)
+    return JSXSyntaxNode(nodelist)
+
+@register.tag(name='JSXComponentFile')
+def do_jsx_component_file(parser, token):
+    try:
+        tag_name, path = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError(f"{token.contents} tag requires a Component path.")
+
+    # Ensure the path is properly quoted
+    if not (path[0] == path[-1] and path[0] in ('"', "'")):
+        raise template.TemplateSyntaxError(f"Component path must be enclosed in quotes: {path}")
+
+    # Strip the quotes from the path
+    path = path[1:-1]
+
+    return JSXFileComponentNode(path)
+
+@register.tag(name="JSXComponent")
+def do_jsx_component(parser, token):
+    nodelist = parser.parse(("endJSXComponent",))
+    parser.delete_first_token()
+    return JSXInlineComponentNode(nodelist)
 
 
-
-# TODO : Configuration option to choose output location. default: /static/
 # TODO: Add option in tag declaration for naming the JSX Component instead of using random.
