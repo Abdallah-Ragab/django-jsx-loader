05d20de53e7fefe313e9c6b8dba9c0c5eaee42c3
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-28 00:32:34 +0200

    FIX: JSXFileComponentNode() takes no arguments

--- jsxloader/nodes/file_component.py
+++ jsxloader/nodes/file_component.py
@@ -1,2 +1,12 @@
-class JSXFileComponentNode:
-    pass
+from django import template
+from django.template import Context
+
+
+class JSXFileComponentNode(template.Node):
+    def __init__(self, nodelist, *args, **kwargs):
+        super(JSXFileComponentNode, self).__init__(*args, **kwargs)
+        self.nodelist = nodelist
+
+    def render(self, context: Context) -> str:
+        print(f"From JSX Syntax Node: {context.template.name}")
+        return super().render(context)
