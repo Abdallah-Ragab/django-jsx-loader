b9253011787f095b87a2d7359e14d7605799a21e
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-28 00:35:32 +0200

    Add JSXSyntaxNode class to jsx_syntax.py

--- jsxloader/nodes/jsx_syntax.py
+++ jsxloader/nodes/jsx_syntax.py
@@ -1,2 +1,12 @@
-class JSXSyntaxNode:
-    pass
+from django import template
+from django.template import Context
+
+class JSXSyntaxNode(template.Node):
+
+    def __init__(self, nodelist, *args, **kwargs):
+        super(JSXSyntaxNode, self).__init__(*args, **kwargs)
+        self.nodelist = nodelist
+
+    def render(self, context: Context) -> str:
+        print(f"From JSX Syntax Node: {context.template.name}")
+        return self.nodelist.render(context)
