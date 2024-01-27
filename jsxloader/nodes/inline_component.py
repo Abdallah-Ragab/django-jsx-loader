52209f43d05b73335ff84bab384a81b738f0e3a6
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-28 00:34:20 +0200

    FIX: JSXInlineComponentNode() takes no arguments

--- jsxloader/nodes/inline_component.py
+++ jsxloader/nodes/inline_component.py
@@ -4,7 +4,7 @@ from django.template import Context
 
 class JSXInlineComponentNode(template.Node):
     def __init__(self, nodelist, *args, **kwargs):
-        super(JSXFileComponentNode, self).__init__(*args, **kwargs)
+        super(JSXInlineComponentNode, self).__init__(*args, **kwargs)
         self.nodelist = nodelist
 
     def render(self, context: Context) -> str:
