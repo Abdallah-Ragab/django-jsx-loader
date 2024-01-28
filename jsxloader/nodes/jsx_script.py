63d123dc27a964ed8c8f0ae9daf2dd6158255dcc
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-28 06:40:48 +0200

    Refactor JSXScriptNode to include template ID

--- jsxloader/nodes/jsx_script.py
+++ jsxloader/nodes/jsx_script.py
@@ -4,10 +4,8 @@ from ..utils import hash_string
 
 class JSXScriptNode(template.Node):
     def render(self, context: Context) -> str:
-        print(context.template_name)
-        print(context.template.name)
-        print(context.template.origin)
-        print(context.template.origin.name)
-        print(context.template.origin.template_name)
-
+        self.template_id = self.get_template_id(context.template.origin.name)
         return "script"
+
+    def get_template_id(self, template_path):
+        return hash_string(template_path)[:6]
