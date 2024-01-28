37e61c67a362a5605e9fecd13db8340e6b2a0909
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-28 07:01:33 +0200

    Refactor JSXFileComponentNode to inherit from JSXNode

--- jsxloader/nodes/file_component.py
+++ jsxloader/nodes/file_component.py
@@ -1,12 +1,13 @@
 from django import template
 from django.template import Context
+from .jsx import JSXNode
 
-
-class JSXFileComponentNode(template.Node):
+class JSXFileComponentNode(JSXNode):
     def __init__(self, path, *args, **kwargs):
         super(JSXFileComponentNode, self).__init__(*args, **kwargs)
         self.path = path
 
     def render(self, context: Context) -> str:
+        super(JSXFileComponentNode).render(context)
         print(f"From JSX File Component Node: {context.template.name}")
         return self.path
