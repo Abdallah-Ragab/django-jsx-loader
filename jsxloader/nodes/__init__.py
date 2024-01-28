edaa48c502412a0fb234fb6e3eec492659aef25e
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-28 06:21:08 +0200

    Add JSXScriptNode to nodes/__init__.py

--- jsxloader/nodes/__init__.py
+++ jsxloader/nodes/__init__.py
@@ -2,3 +2,4 @@ from .jsx import JsxNode
 from .file_component import JSXFileComponentNode
 from .inline_component import JSXInlineComponentNode
 from .jsx_syntax import JSXSyntaxNode
+from .jsx_script import JSXScriptNode
