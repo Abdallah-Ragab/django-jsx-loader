6d5034cf52ed23bf272ea706879ff7504fc48caf
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-28 06:57:43 +0200

    added node counter into context to find each node index

--- jsxloader/nodes/jsx_script.py
+++ jsxloader/nodes/jsx_script.py
@@ -5,7 +5,9 @@ from ..utils import hash_string
 class JSXScriptNode(template.Node):
     def render(self, context: Context) -> str:
         self.template_id = self.get_template_id(context.template.origin.name)
-        return "script"
+        context["jsx_loader"] = {}
+        context["jsx_loader"]["_counter"] = 0
+        return ""
 
     def get_template_id(self, template_path):
         return hash_string(template_path)[:6]
