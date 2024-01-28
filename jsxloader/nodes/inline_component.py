00822e9f806635f3faa2b3d7c9ab7746ffd52f85
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-28 07:00:27 +0200

    Refactor JSXInlineComponentNode to inherit from JSXNode

--- jsxloader/nodes/inline_component.py
+++ jsxloader/nodes/inline_component.py
@@ -1,12 +1,14 @@
 from django import template
 from django.template import Context
+from .jsx import JSXNode
 
 
-class JSXInlineComponentNode(template.Node):
+class JSXInlineComponentNode(JSXNode):
     def __init__(self, nodelist, *args, **kwargs):
         super(JSXInlineComponentNode, self).__init__(*args, **kwargs)
         self.nodelist = nodelist
 
     def render(self, context: Context) -> str:
+        super(JSXInlineComponentNode).render(context)
         print(f"From JSX Inline Component Node: {context.template.name}")
         return self.nodelist.render(context)
