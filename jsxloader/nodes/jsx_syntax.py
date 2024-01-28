c0d9094261fa459809118c0ffd4fad58c74e2da5
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2024-01-28 06:21:15 +0200

    Add JSX component wrapper to JSXSyntaxNode

--- jsxloader/nodes/jsx_syntax.py
+++ jsxloader/nodes/jsx_syntax.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template import Context
+from ..utils import clean_js_variable_name
 
 class JSXSyntaxNode(template.Node):
 
@@ -7,6 +8,23 @@ class JSXSyntaxNode(template.Node):
         super(JSXSyntaxNode, self).__init__(*args, **kwargs)
         self.nodelist = nodelist
 
+
+    def wrap_jsx_in_component(self) -> str:
+        # component_name = clean_js_variable_name(component_name)
+        component_name = ""
+
+        return """
+        import React from 'react';
+        import ReactDOM from 'react-dom';
+
+        const """ + component_name + """ = () => {
+            return (
+                """ + self.jsx + """
+            );
+        }
+        """
+
     def render(self, context: Context) -> str:
         print(f"From JSX Syntax Node: {context.template.name}")
-        return self.nodelist.render(context)
+        self.jsx = self.nodelist.render(context)
+        self.wrap_jsx_in_component()
